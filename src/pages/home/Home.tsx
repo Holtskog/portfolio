689fe78c97334d6350a6952ba032e33882dac4be
Oyvind Holtskog <oholtskog@hotmail.com>
2022-11-15 15:48:00 +0100

    add loading text

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -7,7 +7,14 @@ function Home() {
 	const url = document.baseURI.includes('github.io')
 		? 'https://holtskog.azurewebsites.net/project'
 		: 'https://localhost:7119/project';
-	const [data] = useFetch<ProjectData>(url, true);
+	const [data, loading] = useFetch<ProjectData>(url, true);
+	if (loading) {
+		return (
+			<div className='loading text-light'>
+				<h1>Loading projects...</h1>
+			</div>
+		);
+	}
 	return (
 		<div className='home'>
 			<section className='projects bg-dark'>
